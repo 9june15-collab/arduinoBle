20bae971849ce5dfb43c1c2f05a1b4d93c0fc4f3
Hello Hello <hellohello@Helloui-noteubug.local>
2025-12-15 01:24:27 +0900

    Remove Scan & Connect button

--- sketch.js
+++ sketch.js
@@ -1,17 +1,12 @@
 // 소문자 (아두이노와 동일하게 입력)
 const SERVICE_UUID = "19b10000-e8f2-537e-4f6c-d104768a1214"; 
 const WRITE_UUID = "19b10001-e8f2-537e-4f6c-d104768a1214"; 
-let writeChar, statusP, connectBtn;
+let writeChar, statusP;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // BLE 연결
-  connectBtn = createButton("Scan & Connect");
-  connectBtn.mousePressed(connectAny);
-  connectBtn.size(120, 30);
-  connectBtn.position(20, 40);
-
   statusP = createP("Status: Not connected");
   statusP.position(22, 60);
 }
